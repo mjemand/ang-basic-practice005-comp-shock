ceb0f6ee56d41fa68b19f5f4d52963a5ee5c2a3c
mjemand <mechle.tamas@gmail.com>
2021-02-05 04:00:47 +0100

    Ready

---
+++ src/app/common/data-row/data-row.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { User } from 'src/app/model/user';
+
+@Component({
+  selector: 'app-data-row',
+  templateUrl: './data-row.component.html',
+  styleUrls: ['./data-row.component.scss']
+})
+export class DataRowComponent implements OnInit {
+
+  @Input() dataRow: User = new User();
+  @Output() selectClick: EventEmitter<User> = new EventEmitter();
+  @Output() updateClick: EventEmitter<User> = new EventEmitter();
+  @Output() deleteClick: EventEmitter<User> = new EventEmitter();
+
+  constructor() { }
+
+  onSelectClicked(): void {
+    this.selectClick.emit(this.dataRow)
+  }
+  onUpdateClicked(): void {
+    this.updateClick.emit(this.dataRow)
+  }
+  onDeleteClicked(): void {
+    this.deleteClick.emit(this.dataRow)
+  }
+
+  ngOnInit(): void {
+  }
+
+}
